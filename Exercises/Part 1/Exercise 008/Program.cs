00cdafc05cc1de851a870093359ddfada3592691
lanafabscoding <lanafabscoding@yandex.com>
2020-11-01 13:16:29 +0200

    1.11.2020

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,7 +6,13 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            // create a program that asks user for thei name and greets them. 
+            //let us create the first the line of code that asks for the users name
+            Console.WriteLine("What is your name?");
+            //create a string variable and get users input.
+            string userInput = Console.ReadLine();
+            //greet the user 
+            Console.WriteLine("Hello " + userInput + "!");
 
         }
     }
