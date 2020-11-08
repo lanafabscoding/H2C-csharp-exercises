aa7035061280b9d23699efb42e7dbc651f9eff7a
lanafabscoding <lanafabscoding@yandex.com>
2020-11-08 14:05:41 +0200

    8.11.2020

--- Exercises/Part 1/Exercise 012/Program.cs
+++ Exercises/Part 1/Exercise 012/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_12
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+     //ask a number from the user 
+     Console.Write("Give a number! " +"\n");
+     
+     //declare and assing the users input as a string
+     string userInput = Console.ReadLine();
+
+     //convert string to integer type
+     int intValue = Convert.ToInt32(userInput);
+     
+     //show the answer to the user
+     Console.WriteLine("You gave " + intValue);
+
 
     }
   }
