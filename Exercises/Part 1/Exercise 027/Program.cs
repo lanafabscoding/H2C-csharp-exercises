e26ad0bde0b8f1a179102b9f14ecdac1c1b4cca1
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 14:54:54 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,8 +6,23 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      // ask a number from the user
+      Console.WriteLine("Give a number:");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+     //Convert string to integer type
+     int number = Convert.ToInt32(userInput);
+     //show the users input 
+     if (number > 0)
+     {
+         Console.WriteLine("It is positive");
+     }
+     else
+     {
+         Console.WriteLine("It is not positive");
+     }
+    }
 
     }
   }
-}
+
