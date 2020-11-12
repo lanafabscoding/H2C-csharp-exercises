5d8edd0f0c380fe850cb1688a22faaac8cf298ef
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 15:28:06 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,8 +6,35 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      // ask a number from the user
+      Console.WriteLine("Give the first number!");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+      //Convert string to integer type
+      int number = Convert.ToInt32(userInput);
+      // ask another number from the user
+      Console.WriteLine("Give the second number!");
+      //Deaclare and assing the input as a string
+      string userInput2 = Console.ReadLine();
+      //Convert string to integer type
+      int number2 = Convert.ToInt32(userInput2); 
+    
+    //let us build up the conditions
+    if (number > number2)
+     {
+           Console.WriteLine("The larger number is " + number);
+     }
+    else if (number < number2) 
+     {
+           Console.WriteLine("The larger number is " + number2);
 
     }
+
+    else 
+     {
+            Console.WriteLine("They are equal!");   
+     }
+   
+   }
   }
 }
