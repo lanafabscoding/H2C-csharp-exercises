502166dd0a5b5428dd0175aafb857ac259b917ee
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 08:53:27 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,32 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //ask a string from the user
+       Console.Write("Give a string: ");
+      //set the users input into a string variable
+       string userInput = Console.ReadLine();
+      
+      
+      //ask an integer from the user
+      Console.Write("Give an integer: ");
+     // Declare and assign the input as string
+     string userInput2 = Console.ReadLine();
+     // Convert string to integer type
+     int intValue = Convert.ToInt32(userInput2);
+     
+     //ask a double from the user
+      Console.Write("Give an double: ");
+      string userInput3 = Console.ReadLine();
+     // Convert string to double type
+     double doubleValue = Convert.ToDouble(userInput3);
+     
+     //ask a boolean from the user
+      Console.Write("Give a boolean: ");
+     string userInput4 = Console.ReadLine();
+      string truth = "True";
+      bool booleanValue = System.Convert.ToBoolean(truth);
+
+      Console.WriteLine("Your string: " + userInput +"\n" + "Your integer: " + intValue +"\n" + "Your double: " + doubleValue + "\n" + "Your boolean: " + booleanValue);
 
     }
   }
