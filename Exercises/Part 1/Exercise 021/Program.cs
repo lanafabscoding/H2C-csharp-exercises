d964cd54773dc88582051b09eaaf8260d19d1b3a
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 10:32:24 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //ask a number from the user
+     Console.WriteLine("Give the first number! ");
+     // Declare and assign the input as string
+     string userInput = Console.ReadLine();
+     // Convert string to integer type
+     int firstNumber = Convert.ToInt32(userInput);
+     //ask for the second number
+      Console.WriteLine("Give the second number! ");
+      // Declare and assign the input as string
+     string userInput2 = Console.ReadLine();
+     // Convert string to integer type
+     int secondNumber = Convert.ToInt32(userInput2);
+
+     //make the calculation and print it to the user
+     double answer = (firstNumber + secondNumber)/2.0;
+     //print the equation tot the user.
+     Console.WriteLine("The average is " + answer);
 
     }
   }
