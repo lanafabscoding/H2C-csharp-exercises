e90e71f1c02d07741b5b2bd70fe91928242c7ae5
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 14:20:58 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -7,8 +7,17 @@ namespace exercise_24
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-      
+      // ask the speed from the user
+      Console.WriteLine("Your speed:");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+     //Convert string to integer type
+     int speed = Convert.ToInt32(userInput);
+     //show the users input 
+     if (speed > 120)
+     {
+         Console.WriteLine("Speeding!");
+     }
     }
   }
 }
