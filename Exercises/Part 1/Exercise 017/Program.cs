d35f5316eff0124a17656a5e60ac47f5dadbae4f
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 09:59:01 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //ask a number from the user
+     Console.WriteLine("Give the first number! ");
+     // Declare and assign the input as string
+     string userInput = Console.ReadLine();
+     // Convert string to integer type
+     int firstNumber = Convert.ToInt32(userInput);
+     //ask for the second number
+      Console.WriteLine("Give the second number! ");
+      // Declare and assign the input as string
+     string userInput2 = Console.ReadLine();
+     // Convert string to integer type
+     int secondNumber = Convert.ToInt32(userInput2);
+
+     //make the calculation and print it to the user
+     int sum = firstNumber + secondNumber;
+     Console.WriteLine("The sum is " + sum);
+     
 
     }
   }
