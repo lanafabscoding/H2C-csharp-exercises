083cd199a065a88e1b3b8cfa01fa145ed87d7b5e
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 09:37:02 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -6,8 +6,17 @@ namespace exercise_16
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+    //ask number of days from the user
+     Console.WriteLine("How many days? ");
+     // Declare and assign the input as string
+     string userInput = Console.ReadLine();
+     // Convert string to integer type
+     int intValue = Convert.ToInt32(userInput);
+     //make the calculation and print it to the user
+     int daysToSeconds = intValue*24*60*60;
+     Console.WriteLine(daysToSeconds);
+     
+     
 
     }
   }
