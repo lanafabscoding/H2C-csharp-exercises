372b5abbd6c3768ade8f038c3d0c985697ab6947
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 14:30:07 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 025/Program.cs
+++ Exercises/Part 1/Exercise 025/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+       // ask the speed from the user
+      Console.WriteLine("Give a number:");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+     //Convert string to integer type
+     int year = Convert.ToInt32(userInput);
+     //show the users input 
+     if (year == 1984)
+     {
+         Console.WriteLine("Orwell");
+     }
 
     }
   }
