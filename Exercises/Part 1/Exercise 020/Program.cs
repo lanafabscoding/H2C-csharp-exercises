d9807e648fe2500d5ef40eccf76a098c5d8f1aac
lanafabscoding <lanafabscoding@yandex.com>
2020-11-12 10:15:28 +0200

    12.11.2020

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_20
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+     //ask a number from the user
+     Console.WriteLine("Give the first number! ");
+     // Declare and assign the input as string
+     string userInput = Console.ReadLine();
+     // Convert string to integer type
+     int firstNumber = Convert.ToInt32(userInput);
+     //ask for the second number
+      Console.WriteLine("Give the second number! ");
+      // Declare and assign the input as string
+     string userInput2 = Console.ReadLine();
+     // Convert string to integer type
+     int secondNumber = Convert.ToInt32(userInput2);
+
+     //make the calculation and print it to the user
+     int answer = firstNumber * secondNumber;
+     //print the equation tot the user.
+     Console.WriteLine(firstNumber + " * " +secondNumber + " = " + answer);
 
     }
   }
