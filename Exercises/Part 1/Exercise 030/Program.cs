68e4a0b60b740860206096ad7669070d4d599e2c
lanafabscoding <lanafabscoding@yandex.com>
2020-11-13 17:43:06 +0200

    13.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,7 +6,61 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      // ask a number from the user
+      Console.WriteLine("Give your percent [0 -100]");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+      //Convert string to integer type
+      int number = Convert.ToInt32(userInput);
+      
+    //let us build up the conditions
+    if (number < 0)
+     {
+           Console.WriteLine("Impossible");
+     }
+    else if (number < 50) 
+     {
+           Console.WriteLine("Fail");
+
+    }
+
+    else if (number <60) 
+     {
+           Console.WriteLine("Grade: 1");
+
+    }
+
+    else if (number <70) 
+     {
+           Console.WriteLine("Grade: 2");
+
+    }
+
+     else if (number <80) 
+     {
+           Console.WriteLine("Grade: 3");
+
+    }
+
+     else if (number <90) 
+     {
+           Console.WriteLine("Grade: 4");
+
+    }
+
+    
+     else if (number <100) 
+     {
+           Console.WriteLine("Grade: 5");
+
+    }
+
+
+    else 
+     {
+            Console.WriteLine("Outstanding!");   
+     }
+   
 
     }
   }
