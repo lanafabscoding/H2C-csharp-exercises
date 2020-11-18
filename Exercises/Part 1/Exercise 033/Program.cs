0f24d5ed8693f67934c088d84f69ed78ea3af73d
lanafabscoding <lanafabscoding@yandex.com>
2020-11-18 19:15:42 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      // ask a string from the user
+      Console.WriteLine("Give the first string:");
+     //declare and assign the input as a string 
+     string userInput = Console.ReadLine();
+    //ask for the second string 
+     Console.WriteLine("Give the second string:");
+    //declare and assign the second input to a string
+    string userInput2 = Console.ReadLine();
+
+     //let's create the if else clause
+     if (userInput.Equals(userInput2))
+     {
+        Console.WriteLine("Echo!");
+     }
+     else 
+     {
+        Console.WriteLine("Nope!");
+    }
 
     }
   }
