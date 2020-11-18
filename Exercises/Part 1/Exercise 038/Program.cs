7701a05583aa1243c3fc4b6e45aaf9a280ca4a11
lanafabscoding <lanafabscoding@yandex.com>
2020-11-18 20:27:46 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,33 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int sum = 0;
+      // Repeat until the block has been exited
+    while (true) 
+    {
+
+    // Ask for integers
+    Console.WriteLine("Give a number:");
+    // Read user input
+    int number = Convert.ToInt32(Console.ReadLine());
+    // If the input is 0, exit the loop
+    if (number == 0) 
+    {
+        break;
+    }
+    // If the input is less than 0
+    if  (number <0)
+    {
+        // Count the negative numbers
+        sum = sum + 1;
+    }
+
+    
+     
+}
+
+        // Print the sum
+        Console.WriteLine("Total amount of negative numbers: " + sum);
 
     }
   }
