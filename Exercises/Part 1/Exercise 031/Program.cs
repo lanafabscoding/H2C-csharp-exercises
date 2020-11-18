4b0c3a77edd910c71fe7eecbb3afcb45548c53fc
lanafabscoding <lanafabscoding@yandex.com>
2020-11-18 19:01:02 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,8 +6,21 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      // ask a number from the user
+      Console.WriteLine("Give a number:");
+      //Deaclare and assing the input as a string
+      string userInput = Console.ReadLine();
+      //Convert string to integer type
+      int number = Convert.ToInt32(userInput);
+      //built the if else clause to check if which answer to give 
+     if (number % 2 == 0) 
+     {
+         Console.WriteLine("It is even."); 
+     }
+     else 
+     {
+        Console.WriteLine("It is odd.");
+     }
     }
   }
 }
