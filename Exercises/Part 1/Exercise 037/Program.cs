042cec0382fe9b504b7e9b30c5a2de2342bcca45
lanafabscoding <lanafabscoding@yandex.com>
2020-11-18 20:16:14 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -7,7 +7,29 @@ namespace exercise_37
     public static void Main(string[] args)
     {
       int sum = 0;
-      // Write your code here:
+      // Repeat until the block has been exited
+    while (true) 
+    {
+
+    // Ask for integers
+    Console.WriteLine("Give a number:");
+    // Read user input
+    int number = Convert.ToInt32(Console.ReadLine());
+    // If the input is 0, exit the loop
+    if (number == 0) 
+    {
+        break;
+    }
+    // If the input other thab 0 
+    else  
+    {
+        // Increase the value of countOnes by 1
+        sum = sum + 1;
+    }
+}
+
+// Print the sum
+Console.WriteLine("Total amount of numbers: " + sum);
 
     }
   }
