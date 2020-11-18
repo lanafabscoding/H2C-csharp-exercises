9bc8bd1a1d3ae0979391167b3741ad3df746f641
lanafabscoding <lanafabscoding@yandex.com>
2020-11-18 20:02:50 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,29 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+       // while loop with 0 being the number that ends the program
+      while (true)
+      {
+       //Ask number from user
+       Console.WriteLine("Give a number:");
+       //read the input 
+        int number = Convert.ToInt32(Console.ReadLine());
+       //if number is 0 quit the program
+        if (number == 0)
+        {
+         break;
+        }
+        //if number is less than 0 give error
+        else if (number < 0)   
+        {
+           Console.WriteLine("That is negative");  
+           continue;  
+        }
+       else 
+       {
+        Console.WriteLine(number*number);
+       }
+      }
     }
   }
 }
