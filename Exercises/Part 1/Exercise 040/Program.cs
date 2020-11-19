aac8e791caa6a97a5d11e2e1fc3842214d2bbddd
lanafabscoding <lanafabscoding@yandex.com>
2020-11-19 19:22:48 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -7,7 +7,38 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+       int sum = 0;
+       int amount= 0;
+      // Repeat until the block has been exited
+    while (true) 
+    {
+
+    // Ask for integers
+    Console.WriteLine("Give a number:");
+    // Read user input
+    int number = Convert.ToInt32(Console.ReadLine());
+    // If the input is 0, exit the loop
+    if (number == 0) 
+    {
+        break;
+    }
+    // If the input is other than 0
+    else
+    {
+        // Count the numbers
+        sum = sum + number;
+        amount = amount + 1;
+    }
+
+    
+     
+}
+
+        // Print the sum
+        Console.WriteLine("Total sum of numbers: " + sum);
+        //Print the amount
+        Console.WriteLine("Total amount of numbers:" + amount);
+
     }
   }
 }
