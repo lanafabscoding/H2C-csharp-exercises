cdea11c90ede3df28e6079899ba9c760961bd2ee
lanafabscoding <lanafabscoding@yandex.com>
2020-11-21 10:57:56 +0200

    21.11.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+         //declare variables, assign values, read the users input and convert to integer
+      int firstNumber = Convert.ToInt32(Console.ReadLine());
+        //declare variables, assign values, read the users input and convert to integer
+      int secondNumber = Convert.ToInt32(Console.ReadLine());
+      //if the firstNumber is greater than secondNumber, print it
+      if (firstNumber > secondNumber)
+      {
+          Console.WriteLine(firstNumber + " is greater than " + secondNumber + ".");
+      }
+      //if the firstNumber is less than SeconNumber, print it
+      else if (firstNumber < secondNumber)
+
+      {
+          Console.WriteLine(firstNumber + " is less than " + secondNumber+ "."); 
+      }
+      //any other case print: numbers are equal
+      else 
+      {
+       Console.WriteLine(firstNumber+ " is equal to " + secondNumber + ".");
+      }
 
     }
   }
