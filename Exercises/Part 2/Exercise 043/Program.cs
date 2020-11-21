a850323b476edb4c448e832ac1eeb250c3d7fdfa
lanafabscoding <lanafabscoding@yandex.com>
2020-11-21 10:48:51 +0200

    21.11.2020

--- Exercises/Part 2/Exercise 043/Program.cs
+++ Exercises/Part 2/Exercise 043/Program.cs
@@ -6,8 +6,18 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-      
+       //declare variables, assign values, read the users input and convert to integer
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      //if the number is less than 0, print multiplied by 1
+      if (userInput < 0)
+      {
+          Console.WriteLine(userInput*-1);
+      }
+      else 
+      {
+          Console.WriteLine(userInput); 
+      }
+     
     }
   }
 }
