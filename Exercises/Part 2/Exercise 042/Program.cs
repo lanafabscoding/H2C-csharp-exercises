1bec5dfadcb801fcbfcdaf4fd5f574cac3403c22
lanafabscoding <lanafabscoding@yandex.com>
2020-11-21 10:43:19 +0200

    21.11.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+        //declare variables, assign values, read the users input and convert to integer
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      int userInput2 = Convert.ToInt32(Console.ReadLine());
+      // Identifying the operation and declaring a variable for the result
+      double solution = Math.Sqrt(userInput+userInput2);
+     // Printing the result of the operation
+     Console.WriteLine(solution);
     
     }
   }
