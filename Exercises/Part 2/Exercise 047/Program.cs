b291fc3d0be1733781b1e36ad9d928f9c9abe00c
lanafabscoding <lanafabscoding@yandex.com>
2020-11-26 13:41:55 +0200

    26.11.2020

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -7,8 +7,26 @@ namespace exercise_47
     public static void Main(string[] args)
     {
 
-      // Write your code here:
       
+        // Ask for starting value
+    Console.WriteLine("Where to?");
+     //declare variable, assign value, read the users input and convert it to integer
+      int userInputEnd = Convert.ToInt32(Console.ReadLine());
+       //ask for ending value
+      Console.WriteLine("Where from?");
+      int userInputStart = Convert.ToInt32(Console.ReadLine());
+      //set the userInputStart as a starting value of the for loop 
+      int start= userInputStart;
+     //set userInputEnd as an end value for the for loop
+      int end = userInputEnd;
+       //create the for loop and print it out 
+      for (int i = start; i <= end; i++)
+
+           {
+            //print number from start to end
+            Console.WriteLine(i);
+          
+            }
     }
   }
 }
