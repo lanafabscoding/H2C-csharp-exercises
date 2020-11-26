ee3e065df5189434401d7fc99a03fa6caccbc060
lanafabscoding <lanafabscoding@yandex.com>
2020-11-26 13:19:00 +0200

    26.11.2020

--- Exercises/Part 2/Exercise 046/Program.cs
+++ Exercises/Part 2/Exercise 046/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_46
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+         //declare variable, assign value, read the users input and convert it to integer
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      //set the userInput as a starting value of the for loop 
+      int start= userInput;
+     //set 100 as an end value for the for loop
+      int end = 100;
+       //create the for loop and print it out 
+      for (int i = start; i <= end; i++)
+
+           {
+
+            Console.WriteLine(i);
+          
+            }
 
     }
   }
