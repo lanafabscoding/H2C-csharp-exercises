dd668960369f9aa9eeccd74e72d7353b64fc9626
lanafabscoding <lanafabscoding@yandex.com>
2020-11-26 12:52:29 +0200

    26.11.2020

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -7,8 +7,19 @@ namespace exercise_45
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //declare variable, assign value, read the users input and convert to integer
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      int number= 0;
 
+      //the userInput needs to be less than or equal to number. Then the number gets to be looped from 0 to the userInput. 
+      while (number <= userInput)
+           {
+
+            Console.WriteLine(number);
+            number++;
+             //int userInput = Convert.ToInt32(Console.ReadLine());
+            }
+      
     }
   }
 }
