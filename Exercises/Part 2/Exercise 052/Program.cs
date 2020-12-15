b9e8f1ca7d5c22e6b63977f93d21b0bcdbeac3e2
lanafabscoding <lanafabscoding@yandex.com>
2020-12-15 19:12:22 +0200

    15.12.2020

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -14,6 +14,17 @@ namespace exercise_52
 
     // Write your method here:
 
-
+        public static void PrintFromNumberToOne(int numberOfTimes)
+             {
+            int i = numberOfTimes; 
+            
+        while (i > 0) {
+             
+        
+      
+      Console.WriteLine(i);
+       i--;
+      }
+    }
   }
 }
