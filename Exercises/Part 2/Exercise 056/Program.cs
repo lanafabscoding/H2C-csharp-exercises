a5cbf2331c56e3fe85b3e9744ca86c0afc679bfa
lanafabscoding <lanafabscoding@yandex.com>
2020-12-16 19:09:28 +0200

    16.12.2020

--- Exercises/Part 2/Exercise 056/Program.cs
+++ Exercises/Part 2/Exercise 056/Program.cs
@@ -6,10 +6,15 @@ namespace exercise_56
   {
     public static void Main(String[] args)
     {
-
+     //here the return value gets to be stored in a variable and called
+      string word = Word();
+      Console.WriteLine(word);
     }
-
-    // Write your method here:
-
+    //Here is the method 
+    public static string Word()
+    {
+    //here is the return value
+    return "Hienoa työtä!";
+    }
   }
 }
